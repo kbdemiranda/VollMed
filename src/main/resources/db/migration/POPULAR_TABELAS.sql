052407ad203f0e20e5f8a046e713e3bfdaf43fef
Kaique de Miranda <kbdemiranda@hotmail.com>
2023-04-22 17:58:07 -0300

    Spring Security
    Implementando Token JWT
    Implementando leitura de header
    Implementando Filtros

---
+++ src/main/resources/db/migration/POPULAR_TABELAS.sql
@@ -0,0 +1,19 @@
+insert into vollmed.medicos (nome, email, crm, especialidade, logradouro, numero, complemento, bairro, cidade, uf, cep)
+values ('Aline Sueli Eliane da Paz','aline.sueli.dapaz@voll.med','SP1234','ORTOPEDIA','Rua Doutor Roberto Udihara','615','APTO 21','Jardim Carombé','São Paulo','SP','02855050');
+
+insert into vollmed.medicos (nome, email, crm, especialidade, logradouro, numero, complemento, bairro, cidade, uf, cep)
+values ('Joana Darc','joana.darc@voll.med','SP1122','CARDIOLOGIA','Rua Maria Míriam','615','APTO 123','Cidade de Deus','São Paulo','SP','08081670');
+
+insert into vollmed.medicos (nome, email, crm, especialidade, logradouro, numero, complemento, bairro, cidade, uf, cep)
+values ('Milena Soares','milena.soares@voll.med','SP2243','GINECOLOGIA','Rua Ernesto Cerf Filho','121', null,'Vila Medeiros','São Paulo','SP','02220210');
+
+insert into vollmed.medicos (nome, email, crm, especialidade, logradouro, numero, complemento, bairro, cidade, uf, cep)
+values ('Márcia Helena Ribeiro','marcia.ribeiro@voll.med','SP1122','CARDIOLOGIA','Rua Patrick Bruce','987', null,'Vila Brasílio Machado','São Paulo','SP','04288020');
+
+insert into vollmed.pacientes (nome, email, telefone, cpf, logradouro, numero, complemento, bairro, cidade, uf, cep)
+VALUES ('Maria Eduarda Santos', 'maria.duda@hotmail.com','+5511986122712', '02996566084','Rua Jumana', '349',null, 'Mooca','São Paulo', 'SP', '03121970');
+
+insert into vollmed.pacientes (nome, email, telefone, cpf, logradouro, numero, complemento, bairro, cidade, uf, cep)
+VALUES ('Pedro Augusto do Nascimento', 'pedro.nascimento@hotmail.com','+5511983219999', '75293785036','Travessa Manoel Soares', '121', 'CASA 2', 'Jardim Piratininga','São Paulo', 'SP', '03716200');
+
+insert into vollmed.usuarios (login, senha) values('ana.souza@voll.med', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.');
